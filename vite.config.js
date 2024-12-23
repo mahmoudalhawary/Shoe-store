04384029067f88e58f4563fd5fcba5d247c3faad
mahmoud <108419357+mahmoudnasr20@users.noreply.github.com>
2024-12-24 01:05:42 +0200

    Update vite.config.js to set correct base path for deployment 2

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,18 @@
 import { defineConfig } from 'vite';
-// import react from '@vitejs/plugin-react';
-
-// export default defineConfig({
-//     plugins: [react()],
-//     resolve: {
-//         alias: {
-//             '@': '/src',
-//         },
-//     },
-// });import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+    plugins: [react()],
+    resolve: {
+        alias: {
+            '@': '/src',
+        },
+    },
     base: '/Shoe-store/',
+
 });
+// import { defineConfig } from 'vite';
+
+// export default defineConfig({
+//     base: '/Shoe-store/',
+// });
